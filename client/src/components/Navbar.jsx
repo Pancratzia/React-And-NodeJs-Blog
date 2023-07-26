4fec6c97a9230f6cf34d0b7ffb614d9c3facf41b
Pancratzia <le.ortegaperez@gmail.com>
2023-07-26 18:37:53 -0400

    Se crea el metodo en Home para cargar los posts

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,66 +1,73 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Logo from '../img/logo.png';
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 
 function Navbar() {
-
   const links = [
     {
-      name: 'ART',
-      link: '/?cat=art'
+      name: "ART",
+      link: "/?cat=art",
     },
     {
-      name: 'SCIENCE',
-      link: '/?cat=science'
+      name: "SCIENCE",
+      link: "/?cat=science",
     },
     {
-      name: 'TECHNOLOGY',
-      link: '/?cat=technology'
+      name: "TECHNOLOGY",
+      link: "/?cat=technology",
     },
     {
-      name: 'CINEMA',
-      link: '/?cat=cinema'
+      name: "CINEMA",
+      link: "/?cat=cinema",
     },
     {
-      name: 'DESIGN',
-      link: '/?cat=design'
+      name: "DESIGN",
+      link: "/?cat=design",
     },
     {
-      name: 'FOOD',
-      link: '/?cat=food'
-    }
-  ]
+      name: "FOOD",
+      link: "/?cat=food",
+    },
+  ];
 
-  const { currentUser, logout } = useContext(AuthContext); 
+  const { currentUser, logout } = useContext(AuthContext);
 
-    return (
-      <div className="navbar">
-        <div className="container">
-
-          <div className="logo">
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
             <img src={Logo} alt="logo" />
-          </div>
+          </Link>
+        </div>
 
-          <div className="links">
+        <div className="links">
+          {links.map((link) => (
+            <Link className="link" key={link.name} to={link.link}>
+              {link.name}
+            </Link>
+          ))}
 
-            {links.map(link => (
-              <Link className='link' key={link.name} to={link.link}>{link.name}</Link>
-            ))}
-
-            <span>{currentUser?.username}</span>
-            { currentUser ? <span onClick={logout}>Logout</span> : <Link className='link' to="/login">Login</Link>}
-            <span className="write">
-              <Link className='link' to="/write">Write</Link>
-            </span>
-
-          </div>
-
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
         </div>
       </div>
-    );
-  }
-  
-  export default Navbar;
+    </div>
+  );
+}
+
+export default Navbar;
